5aa5eb5ccc75ca800d1605b3c16f273ff3825db7
Android_Dave <darvds@gmail.com>
2016-04-03 17:21:38 +0100

    Refactored view mode

---
+++ shinywatchface/src/main/java/com/darvds/shinywatchface/model/DisplayMode.java
@@ -0,0 +1,15 @@
+package com.darvds.shinywatchface.model;
+
+/**
+ * The different available modes that the watch face can display
+ */
+public class DisplayMode {
+
+    public static final int TIME = 0;
+
+    public static final int SECONDS = 1;
+
+    public static final int DATE = 2;
+
+
+}
